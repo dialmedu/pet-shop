e8867b43d9f0ab298c0bd23995decfb25f30c9fc
JeiDevp <joseanyeloserrano@gmail.com>
2020-05-10 13:54:18 -0400

    Initialization

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+   
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+
+  }
+})
+</script>
+
+<style>
+
+</style>
